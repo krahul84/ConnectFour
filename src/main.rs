28aec8c4e09969c243412fc217d9c1d7e7c5c786
Rahul Kumar <krahul@pdx.edu>
2018-08-12 22:31:29 -0700

    Main File Commit

---
+++ src/main.rs
@@ -0,0 +1,322 @@
+use std::default::Default;
+use std::fmt;
+use std::io::stdin;
+use std::str::FromStr;
+
+
+pub struct GridMatrix{
+     row_size:usize,
+     col_size:usize,
+     Values: Vec<Box>,
+
+}
+
+impl GridMatrix{
+    pub fn createGrid(row_size:usize,col_size:usize) -> GridMatrix{
+       GridMatrix{
+         row_size: row_size,
+         col_size: col_size,
+         Values: (0..row_size).map(|_| Box::create(col_size)).collect(),
+      }
+    }
+   
+   pub fn grid_Matrix(&mut self, player: Grid, column: usize) -> Result<(), ()> {
+        let exception = Err(());
+
+        for value in self.Values.iter_mut().rev() {
+            if value.position_grid(player, column).is_ok() {
+                return Ok(());
+            }
+        }
+
+        exception
+    }
+
+     pub fn grid_print(&self) {
+        println!("{}", self);
+    }
+
+    fn winner_rowwise(&self) -> Option<Grid> {
+        let mut num = 1;
+        let mut grid_last = Grid::Empty;
+
+        for value in self.Values.iter() {
+            for value in value.grid.iter() {
+                if value.check_player() && *value == grid_last {
+                    num += 1;
+
+                    if num == 4 {
+                        return Some(*value)
+                    }
+                } else {
+                    num = 1;
+                    grid_last = *value;
+                }
+            }
+        }
+
+        None
+    }
+
+    fn winner_columnwise(&self) -> Option<Grid> {
+        let mut num = 1;
+        let mut grid_last = Grid::Empty;
+
+        for column in (0..self.col_size) {
+            for value in self.Values.iter() {
+                let gridval = value.grid[column];
+
+                if gridval.check_player() && gridval == grid_last {
+                    num += 1;
+
+                    if num == 4 {
+                        return Some(gridval)
+                    }
+                } else {
+                    num = 1;
+                    grid_last = gridval;
+                }
+            }
+        }
+
+        None
+    }
+
+      fn winner_daigonal_down(&self) -> Option<Grid> {
+        for (cols, _) in (0..(self.col_size - 3)).enumerate() {
+            for (rows, _) in (0..(self.row_size - 3)).enumerate() {
+                let mut num = 1;
+                let mut grid_last = self.Values[rows].grid[cols];
+
+                if !grid_last.check_player() {
+                    continue;
+                }
+
+                for offset in (1..4) {
+                    let gridval = self.Values[rows + offset].grid[cols + offset];
+
+                    if gridval.check_player() && gridval == grid_last {
+                        num += 1;
+
+                        if num == 4 {
+                            return Some(gridval);
+                        }
+                    } else {
+                        num = 1;
+                        grid_last = gridval;
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
+     fn winner_daigonal_up(&self) -> Option<Grid> {
+        for (cols, _) in (0..(self.col_size - 3)).enumerate() {
+            for (rows, _) in ((self.row_size - 3)..self.row_size).enumerate() {
+                let row_num = self.row_size - 3 + rows;
+                let mut num = 1;
+                let mut grid_last = self.Values[row_num].grid[cols];
+
+                if !grid_last.check_player() {
+                    continue;
+                }
+
+                for offset in (1..4) {
+                    let gridval = self.Values[row_num - offset].grid[cols + offset];
+
+                    if gridval.check_player() && gridval == grid_last {
+                        num += 1;
+
+                        if num == 4 {
+                            return Some(gridval);
+                        }
+                    } else {
+                        num = 1;
+                        grid_last = gridval;
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
+      pub fn winner(&self) -> Option<Grid> {
+        match self.winner_rowwise() {
+            Some(tile) => Some(tile),
+            None => match self.winner_columnwise() {
+                Some(tile) => Some(tile),
+                None => match self.winner_daigonal_down() {
+                    Some(tile) => Some(tile),
+                    None => match self.winner_daigonal_up() {
+                        Some(tile) => Some(tile),
+                        None => None,
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+impl fmt::Display for GridMatrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        for n in 0..self.Values[0].size() {
+            try!(write!(f, "{}  ", n + 1));
+        }
+
+        try!(writeln!(f, ""));
+
+        let mut result = Ok(());
+
+        for row in self.Values.iter() {
+            result = writeln!(f, "{}", row);
+        }
+
+        result
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Grid {
+    Empty,
+    A,
+    B,
+}
+
+impl Default for Grid {
+    fn default() -> Grid {
+        Grid::Empty
+    }
+}
+impl Grid {
+    pub fn check_player(&self) -> bool {
+        match *self {
+            Grid::Empty => false,
+            _ => true,
+        }
+    }
+}
+
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let result = match *self {
+            Grid::Empty => ".",
+            Grid::A => "A",
+            Grid::B => "B",
+        };
+
+        write!(f, "{}", result)
+    }
+}
+
+impl PartialEq for Grid {
+    fn eq(&self, secplayer: &Grid) -> bool {
+        format!("{}", self) == format!("{}", secplayer)
+    }
+}
+
+pub struct Box {
+    grid: Vec<Grid>,
+}
+
+impl Box {
+    pub fn create(col_size: usize) -> Box {
+        Box {
+            grid: (0..col_size).map(|_| Grid::Empty).collect(),
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.grid.len() as usize
+    }
+
+    pub fn position_grid(&mut self, player: Grid, column: usize) -> Result<(), ()> {
+        if column > self.grid.len() - 1 {
+            return Err(());
+        }
+
+        match self.grid[column] {
+            Grid::Empty => {
+                self.grid[column] = player;
+                return Ok(());
+            },
+            _ => return Err(()),
+        }
+    }
+}
+
+impl fmt::Display for Box {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let mut output = Ok(());
+
+        for value in self.grid.iter() {
+            output = write!(f, "{}  ", value);
+        }
+
+        output
+    }
+}
+
+
+fn main() {
+    let mut grid = GridMatrix::createGrid(6, 7);
+    let mut player = Grid::A;
+
+    grid.grid_print();
+
+    loop {
+        println!("Player {}'s turn. Which column? ", player);
+        let mut input = String::new();
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                let column = match usize::from_str(&input[..].trim()) {
+                    Ok(column) => {
+                        if column != 0 {
+                            column - 1
+                        } else {
+                            println!("That isn't a valid column!");
+                            continue;
+                        }
+                    },
+                    Err(_) => {
+                        println!("That isn't a valid column!");
+                        continue;
+                    }
+                };
+
+                if grid.grid_Matrix(player, column).is_err() {
+                    println!("That isn't a valid column!");
+                    continue;
+                }
+
+                grid.grid_print();
+
+                match grid.winner() {
+                    Some(winner) => {
+                        println!("Player {} wins!", winner);
+                        break;
+                    },
+                    None => {},
+                }
+
+                player = match player {
+                    Grid::B => Grid::A,
+                    _ => Grid::B,
+                };
+            },
+            Err(_) => {
+                println!("Input required.");
+                continue;
+            }
+        }
+    }
+}
+
+
+
+
+
+
